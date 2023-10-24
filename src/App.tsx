c6bad7fb9192dd3a98790b051b283eded840e447
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-10-24 19:22:22 +0300

    chore: fix configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import React from 'react';
+
 function App() {
   return (
     <>
-      <div >
-        App app
-      </div>
+      <div>App app</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
