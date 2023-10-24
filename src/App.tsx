79e39b3425622cc960d0ab3919c367dbc2db828f
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-10-25 01:00:33 +0300

    feat: add component update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ class App extends Component {
   render() {
     return (
       <>
-        <div>App app</div>
         <SearchInput />
       </>
     );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,9 +2,47 @@ import React, { ChangeEvent, Component } from 'react';
 
 export default class SearchInput extends Component {
   state = {
-    value: '...',
+    error: null,
+    isLoaded: false,
+    items: [{ name: '', url: '0' }],
+    pageNumber: 1,
     name: '...',
+    value: '...',
   };
+
+  stateUpdate() {
+    fetch(
+      `https://pokeapi.co/api/v2/berry/?limit=10&offset=${
+        (this.state.pageNumber - 1) * 10
+      }`
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.results,
+          });
+          console.log(result);
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  componentDidMount() {
+    this.stateUpdate();
+  }
+
+  componentDidUpdate() {
+    if (!this.state.isLoaded) this.stateUpdate();
+  }
+
+  componentWillUnmount() {}
 
   handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     this.setState({
@@ -14,16 +52,22 @@ export default class SearchInput extends Component {
 
   handleButtonClick = () => {
     this.setState({
-      name: this.state.value,
+      pageNumber: this.state.value,
+      isLoaded: false,
     });
   };
 
   render() {
     return (
       <>
-        <div>{this.state.name}</div>
         <input onChange={this.handleChange} />
         <button onClick={this.handleButtonClick}>Click</button>
+        <div>{this.state.error ? 'Error' : ''}</div>
+        {!this.state.isLoaded ? (
+          <div>LOADING</div>
+        ) : (
+          this.state.items.map((e) => <div key={e.url}>{e.name}</div>)
+        )}
       </>
     );
   }
