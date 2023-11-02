3f5abe450f97fa2f553edf561d420076eee455af
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-11-03 02:05:54 +0300

    feat: change SearchInput to functional component

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,47 +1,35 @@
-import React, { ChangeEvent, Component, KeyboardEvent } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useEffect, useState } from 'react';
 
-export default class SearchInput extends Component<{
-  setName: (str: string) => void;
-}> {
-  state = {
-    value: '',
+export default function SearchInput(props: { setName: (str: string) => void }) {
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    const searchValue = localStorage.getItem('search-value');
+    const str = searchValue ? searchValue : '';
+    setValue(str);
+  }, []);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value ? e.target.value : '');
   };
 
-  componentDidMount() {
-    this.setState({
-      value: localStorage.getItem('search-value')
-        ? localStorage.getItem('search-value')
-        : '',
-    });
-  }
-
-  handleChange(e: ChangeEvent<HTMLInputElement>) {
-    this.setState({
-      value: e.target.value ? e.target.value : '',
-    });
-  }
-
-  handleSubmit() {
-    this.props.setName(this.state.value);
-    localStorage.setItem('search-value', this.state.value);
-  }
-
-  handleKeyDown(e: KeyboardEvent) {
-    if (e.key === 'Enter') {
-      this.handleSubmit();
+  const handleSubmit = () => {
+    if (value !== null) {
+      props.setName(value);
+      localStorage.setItem('search-value', value);
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="search">
-        <input
-          value={this.state.value}
-          onChange={this.handleChange.bind(this)}
-          onKeyDown={this.handleKeyDown.bind(this)}
-        />
-        <button onClick={this.handleSubmit.bind(this)}>Search</button>
-      </div>
-    );
-  }
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+  };
+
+  return (
+    <div className="search">
+      <input value={value} onChange={handleChange} onKeyDown={handleKeyDown} />
+      <button onClick={handleSubmit}>Search</button>
+    </div>
+  );
 }
