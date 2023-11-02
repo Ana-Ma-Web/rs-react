38aad68111103f4600c0647f397721aa7191aea1
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-11-03 02:09:05 +0300

    feat: change SearchResults to functional component

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { SearchItem } from '../types';
 
-export default class SearchResults extends Component<{
-  items: SearchItem[] | null;
-}> {
-  render() {
-    return (
-      <div className="results">
-        {this.props.items === null || this.props.items === undefined ? (
-          <div>NOT FOUND</div>
-        ) : (
-          this.props.items.map((e) => (
-            <div className="card" key={e.url}>
-              <div className="card__name">{e.name}</div>
-              <div className="card__description">
-                <div className="card__date">Date: {e.air_date}</div>
-                <div className="card__episode">Episode: {e.episode}</div>
-              </div>
+export default function SearchResults(props: { items: SearchItem[] | null }) {
+  return (
+    <div className="results">
+      {props.items === null || props.items === undefined ? (
+        <div>NOT FOUND</div>
+      ) : (
+        props.items.map((e) => (
+          <div className="card" key={e.url}>
+            <div className="card__name">{e.name}</div>
+            <div className="card__description">
+              <div className="card__date">Date: {e.air_date}</div>
+              <div className="card__episode">Episode: {e.episode}</div>
             </div>
-          ))
-        )}
-      </div>
-    );
-  }
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
